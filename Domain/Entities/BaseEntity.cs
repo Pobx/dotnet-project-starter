68090458bb90d117c7c703852a06abb6c231cd6c
11430279 <geidtiphong@gmail.com>
2022-11-22 09:22:40 +0700

    feat: update default value for datetime

--- Domain/Entities/BaseEntity.cs
+++ Domain/Entities/BaseEntity.cs
@@ -2,9 +2,9 @@
 public class BaseEntity
 {
     public long Id { get; set; }
-    public DateTime CreatedTimestamp { get; set; }
+    public DateTime CreatedTimestamp { get; set; } = DateTime.Now;
     public string CreatedBy { get; set; } = String.Empty;
-    public DateTime UpdatedTimestamp { get; set; }
+    public DateTime UpdatedTimestamp { get; set; } = DateTime.Now;
     public string UpdatedBy { get; set; } = String.Empty;
 }
 
