c3f835fdb70fd87ea045c868255fb5964bf8feec
11430279 <geidtiphong@gmail.com>
2022-11-22 21:15:13 +0700

    feat: edit name properties

--- Domain/Entities/BaseEntity.cs
+++ Domain/Entities/BaseEntity.cs
@@ -2,9 +2,9 @@
 public class BaseEntity
 {
     public long Id { get; set; }
-    public DateTime CreatedTimestamp { get; set; } = DateTime.Now;
+    public DateTime CreatedDate { get; set; } = DateTime.Now;
     public string CreatedBy { get; set; } = String.Empty;
-    public DateTime UpdatedTimestamp { get; set; } = DateTime.Now;
+    public DateTime UpdatedDate { get; set; } = DateTime.Now;
     public string UpdatedBy { get; set; } = String.Empty;
 }
 
