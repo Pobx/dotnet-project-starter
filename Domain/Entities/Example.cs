1ce5c1b51b250cb6b0c8bdb02fe9dff125f259f6
11430279 <geidtiphong@gmail.com>
2022-11-22 21:47:17 +0700

    feat: edit for nullable

--- Domain/Entities/Example.cs
+++ Domain/Entities/Example.cs
@@ -6,7 +6,7 @@ namespace Domain.Entities
         public string FirstName { get; set; } = String.Empty;
         public string LastName { get; set; } = String.Empty;
         public int? Age { get; set; }
-        public string Image { get; set; } = String.Empty;
+        public string? Image { get; set; }
     }
 }
 
