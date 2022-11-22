b09f216028fd1fa493c9570fcd48847dbad93b94
11430279 <geidtiphong@gmail.com>
2022-11-22 09:37:36 +0700

    feat: add IGenericRepository

---
+++ Domain/Interfaces/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq.Expressions;
+using Domain.Entities;
+
+namespace Domain.Interfaces
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> expression);
+        Task Add(T entity);
+        Task AddRange(IEnumerable<T> entities);
+        Task Remove(T entity);
+        Task RemoveRange(IEnumerable<T> entities);
+    }
+}
+
