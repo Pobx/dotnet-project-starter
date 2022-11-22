da4732f0e5e83a6ef61324c67cc373820cc44faf
11430279 <geidtiphong@gmail.com>
2022-11-22 20:58:45 +0700

    feat: add response model

---
+++ Domain/DTO/Response/ResponseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Domain.DTO.Response
+{
+    public class ResponseEntity<T>
+    {
+        public T Entity { get; set; }
+        public int CurrentPageIndex { get; set; }
+        public int PageSize { get; set; }
+        public int TotalRows { get; set; }
+        public List<string> Messages { get; set; } = new List<string>();
+    }
+}
+
