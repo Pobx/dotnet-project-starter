1d721eb3a15588b20cd79ddb10e66cde09597a46
11430279 <geidtiphong@gmail.com>
2022-11-22 21:48:21 +0700

    feat: just update migration files

--- WebAPI/Migrations/20221122142733_InitialExample.Designer.cs
+++ WebAPI/Migrations/20221122142733_InitialExample.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using DataAccess.EFCore.PostgreSQL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace WebAPI.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20221122142733_InitialExample")]
+    partial class InitialExample
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
