a79e216365969c9c4ab51a4cc159ec1a02da5514
11430279 <geidtiphong@gmail.com>
2022-12-04 13:50:10 +0700

    feat: configuration AddJsonOptions

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,11 +1,17 @@
 ﻿using DataAccess.EFCore.PostgreSQL;
 using DataAccess.EFCore.SqlServer;
+using Microsoft.AspNetCore.Http.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.WriteIndented = true;
+    options.JsonSerializerOptions.PropertyNamingPolicy = null;
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
