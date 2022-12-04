c89a640ff4530c6b02cefc82c12c6792dc63707a
11430279 <geidtiphong@gmail.com>
2022-12-04 13:28:46 +0700

    feat: edit response entity for nullable

--- Domain/DTO/Response/ResponseEntity.cs
+++ Domain/DTO/Response/ResponseEntity.cs
@@ -3,7 +3,7 @@ namespace Domain.DTO.Response
 {
     public class ResponseEntity<T>
     {
-        public T Entity { get; set; }
+        public T? Entity { get; set; }
         public int CurrentPageIndex { get; set; }
         public int PageSize { get; set; }
         public int TotalRows { get; set; }
