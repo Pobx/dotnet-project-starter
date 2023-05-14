6a24e85b1448ee198c79f9c1105266b39cfb025f
Pobx <geidtiphong.singseewo@krungsrinimble.com>
2023-05-14 10:53:34 +0700

    feat: create ProjectConfiguration class

---
+++ Domain/Utils/ProjectConfiguration.cs
@@ -0,0 +1,12 @@
+
+namespace Domain.Utils
+{
+    public class ProjectConfiguration
+    {
+
+        public const string ProjectName = "ProjectName";
+        public const string DefaultConnectionPostgreSQL = "DefaultConnectionPostgreSQL";
+        public const string DefaultConnectionSqlServer = "DefaultConnectionSqlServer";
+    }
+}
+
