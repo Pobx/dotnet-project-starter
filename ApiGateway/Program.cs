1f9620e3797e99ac48f7dd57ccaa9d24db480ea2
11430279 <geidtiphong.singseewo@krungsrinimble.com>
2023-06-17 21:23:30 +0700

    feat: configuration ocelot for support swagger from downstream

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -1,21 +1,39 @@
+using Microsoft.Extensions.Configuration;
+using MMLib.SwaggerForOcelot.DependencyInjection;
+using Ocelot.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddEndpointsApiExplorer();
+
+var environment = builder.Environment;
+var contentRootPath = builder.Environment.ContentRootPath;
+var ocelotConfigurationFolder = builder.Configuration.GetValue<string>("OcelotConfigurationFolder") ?? throw new ArgumentException("OcelotConfigurationFolder is not in appsetting.json");
+
+builder.Configuration.AddOcelotWithSwaggerSupport(options =>
+{
+    options.Folder = ocelotConfigurationFolder;
+});
+
+builder.Services.AddSwaggerForOcelot(builder.Configuration);
+builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwaggerForOcelotUI(opt =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    opt.PathToSwaggerGenerator = "/swagger/docs";
+    opt.DownstreamSwaggerHeaders = new[]
+  {
+      new KeyValuePair<string, string>("Auth-Key", "AuthValue"),
+  };
+});
 
+// Configure the HTTP request pipeline.
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
